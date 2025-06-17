0a7ef4e0dc18c630dc21dd22c53ccda00313f8d5
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-18 03:43:05 +0900

    Feat: 회원가입 API 엔드포인트 구현

--- src/main/java/kr/co/amateurs/server/controller/auth/AuthController.java
+++ src/main/java/kr/co/amateurs/server/controller/auth/AuthController.java
@@ -3,7 +3,9 @@ package kr.co.amateurs.server.controller.auth;
 import jakarta.validation.Valid;
 import kr.co.amateurs.server.domain.dto.auth.SignupRequestDto;
 import kr.co.amateurs.server.domain.dto.auth.SignupResponseDto;
+import kr.co.amateurs.server.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
     @PostMapping("signup")
     public ResponseEntity<SignupResponseDto> signup(@Valid @RequestBody SignupRequestDto request) {
-        return ResponseEntity.ok().build();
+        SignupResponseDto response = authService.signup(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
