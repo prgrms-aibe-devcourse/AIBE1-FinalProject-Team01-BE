1dc2179476d564a56292f926e8ea6bb935e4758e
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-20 02:42:58 +0900

    Refactor: UserTopicRepository @Repository 어노테이션 제거

--- src/main/java/kr/co/amateurs/server/repository/topic/UserTopicRepository.java
+++ src/main/java/kr/co/amateurs/server/repository/topic/UserTopicRepository.java
@@ -4,6 +4,5 @@ import kr.co.amateurs.server.domain.entity.topic.UserTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserTopicRepository extends JpaRepository<UserTopic, Long> {
 }
