f14f2c84a40e7c75611bd35e5ef578da4b8d965d
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-19 17:08:50 +0900

    Refactor: @Repository 어노테이션 제거

--- src/main/java/kr/co/amateurs/server/repository/user/UserRepository.java
+++ src/main/java/kr/co/amateurs/server/repository/user/UserRepository.java
@@ -2,11 +2,9 @@ package kr.co.amateurs.server.repository.user;
 
 import kr.co.amateurs.server.domain.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
