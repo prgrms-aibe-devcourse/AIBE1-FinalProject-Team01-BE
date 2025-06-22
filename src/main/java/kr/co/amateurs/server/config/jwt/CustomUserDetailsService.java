3d4c7633914f2978093a51354ee9a433fd2e7ce7
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-22 19:53:51 +0900

    Feat: UserDetails 및 UserDetailsService 구현

---
+++ src/main/java/kr/co/amateurs/server/config/jwt/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package kr.co.amateurs.server.config.jwt;
+
+import kr.co.amateurs.server.domain.entity.user.User;
+import kr.co.amateurs.server.repository.user.UserRepository;
+import kr.co.amateurs.server.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+        return new CustomUserDetails(user);
+    }
+}
