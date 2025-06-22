45c3cc1ae7b93ccdc8b21fc8ee9664b84c99fa85
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-23 00:55:39 +0900

    Feat: Spring Security JWT 설정 추가

--- src/main/java/kr/co/amateurs/server/config/SecurityConfig.java
+++ src/main/java/kr/co/amateurs/server/config/SecurityConfig.java
@@ -1,22 +1,55 @@
 package kr.co.amateurs.server.config;
 
+import kr.co.amateurs.server.config.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
-                );
+                )
+
+                /* TODO: 개발 완료 후 아래 설정으로 변경할 예정
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers(
+                                        "/api/auth/**",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui.html",
+                                        "/actuator/**"
+                                ).permitAll()
+
+                                .requestMatchers(HttpMethod.GET,
+                                        "/api/v1/reviews/**",
+                                        "/api/v1/news/**",
+                                        "/api/v1/projects/**"
+                                ).permitAll()
+
+                                .anyRequest().authenticated()
+                )
+                */
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
