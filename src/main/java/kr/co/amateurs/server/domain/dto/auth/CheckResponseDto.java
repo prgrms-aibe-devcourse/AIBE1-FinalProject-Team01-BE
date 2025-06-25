57f82749ef80c51e4e8eeabf5faabf57a6273e93
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-25 21:23:14 +0900

    Fix: CheckResponseDto 오타 수정

--- src/main/java/kr/co/amateurs/server/domain/dto/auth/CheckResponseDto.java
+++ src/main/java/kr/co/amateurs/server/domain/dto/auth/CheckResponseDto.java
@@ -7,7 +7,7 @@ public record CheckResponseDto(
         @Schema(description = "사용 가능 여부", example = "true")
         boolean available,
 
-        @Schema(description = "응답 메세지", example = "사용 가능한 이메일 입니다")
+        @Schema(description = "응답 메시지", example = "사용 가능한 이메일입니다")
         String message
 ) {
     public static CheckResponseDto available(String type) {
