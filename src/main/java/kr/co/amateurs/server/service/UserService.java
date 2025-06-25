e544b0dd1f9db3fb55bd56694750bccea097286e
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-26 02:20:00 +0900

    Fix: 프로필 조회 API Lazy Loading 에러 해결

--- src/main/java/kr/co/amateurs/server/service/UserService.java
+++ src/main/java/kr/co/amateurs/server/service/UserService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -78,10 +79,15 @@ public class UserService {
         }
     }
 
+    @Transactional(readOnly = true)
     public UserProfileResponseDto getCurrentUserProfile() {
-        User currentUser = getCurrentUser()
+        Long userId = getCurrentUser()
+                .map(User::getId)
                 .orElseThrow(ErrorCode.USER_NOT_FOUND);
 
-        return UserProfileResponseDto.from(currentUser);
+        User user = userRepository.findById(userId)
+                .orElseThrow(ErrorCode.USER_NOT_FOUND);
+
+        return UserProfileResponseDto.from(user);
     }
 }
