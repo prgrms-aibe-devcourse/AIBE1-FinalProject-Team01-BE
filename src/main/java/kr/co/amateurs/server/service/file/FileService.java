128e1d5166abc6a388606de7cdc3b5af150a07cd
dldsudqlsdlqslek <tina4686@gmail.com>
2025-06-25 10:43:48 +0900

    refactor: 파일 밸리데이션 추가

--- src/main/java/kr/co/amateurs/server/service/file/FileService.java
+++ src/main/java/kr/co/amateurs/server/service/file/FileService.java
@@ -1,6 +1,8 @@
 package kr.co.amateurs.server.service.file;
 
+import kr.co.amateurs.server.domain.common.ErrorCode;
 import kr.co.amateurs.server.domain.dto.file.FileResponseDTO;
+import kr.co.amateurs.server.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -30,9 +33,7 @@ public class FileService {
     private final S3Client s3Client;
 
     public FileResponseDTO uploadFile(MultipartFile file, String directoryPath) throws IOException {
-        if (file.isEmpty()) {
-            throw new IllegalArgumentException("빈 파일은 업로드할 수 없습니다.");
-        }
+        validateImageFile(file);
 
         String originalFilename = file.getOriginalFilename();
         String extension = "";
@@ -75,6 +76,29 @@ public class FileService {
         } catch (Exception e) {
             log.error("파일 삭제 중 오류 발생: {}", e.getMessage(), e);
             return false;
+        }
+    }
+
+    // TODO - 현재는 타입은 4가지, 용량은 5MB 까지 허용했지만 추가적으로 필요 시 확장 및 변경 가능
+    private static final List<String> ALLOWED_IMAGE_TYPES = List.of(
+            "image/jpeg",  // .jpg, .jpeg 둘 다 포함
+            "image/png",
+            "image/gif",
+            "image/webp"
+    );
+    private static final long MAX_IMAGE_SIZE = 5 * 1024 * 1024;
+    private void validateImageFile(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new CustomException(ErrorCode.EMPTY_FILE);
+        }
+
+        String contentType = file.getContentType();
+        if (contentType == null || !ALLOWED_IMAGE_TYPES.contains(contentType)) {
+            throw new CustomException(ErrorCode.INVALID_FILE_TYPE);
+        }
+
+        if (file.getSize() > MAX_IMAGE_SIZE) {
+            throw new CustomException(ErrorCode.FILE_SIZE_EXCEEDED);
         }
     }
 
