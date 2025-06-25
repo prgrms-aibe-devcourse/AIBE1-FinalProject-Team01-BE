64aeeb0af82c91ae0ecaf1cd0bd2834f157de50c
klaus9267 <klaus9267@gmail.com>
2025-06-25 15:43:16 +0900

    Fix: aop를 사용한 알람 생성 로직 변경으로 인한 facade 삭제

--- src/main/java/kr/co/amateurs/server/controller/alarm/AlarmController.java
+++ src/main/java/kr/co/amateurs/server/controller/alarm/AlarmController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.amateurs.server.domain.dto.alarm.AlarmPageResponse;
 import kr.co.amateurs.server.domain.dto.common.PaginationParam;
-import kr.co.amateurs.server.service.alarm.AlarmFacade;
+import kr.co.amateurs.server.service.alarm.AlarmService;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
@@ -16,25 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AlarmController {
 
-    private final AlarmFacade alarmFacade;
+    private final AlarmService alarmService;
 
     @GetMapping
     @Operation(summary = "알람 목록 조회")
     public ResponseEntity<AlarmPageResponse> readAlarms(@ParameterObject PaginationParam param) {
-        return ResponseEntity.ok(alarmFacade.readAlarms(param));
+        return ResponseEntity.ok(alarmService.readAlarms(param));
     }
 
     @PatchMapping
     @Operation(summary = "알람 전체 읽기 처리")
     public ResponseEntity<Void> markAllAsRead() {
-        alarmFacade.markAllAsRead();
+        alarmService.markAllAsRead();
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("{alarmId}")
     @Operation(summary = "알람 단 건 읽음 처리 ")
     public ResponseEntity<Void> markAsRead(@PathVariable String alarmId) {
-        alarmFacade.markAsRead(alarmId);
+        alarmService.markAsRead(alarmId);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/kr/co/amateurs/server/service/alarm/AlarmService.java
+++ src/main/java/kr/co/amateurs/server/service/alarm/AlarmService.java
@@ -3,7 +3,9 @@ package kr.co.amateurs.server.service.alarm;
 import kr.co.amateurs.server.domain.dto.alarm.AlarmPageResponse;
 import kr.co.amateurs.server.domain.dto.common.PaginationParam;
 import kr.co.amateurs.server.domain.entity.alarm.Alarm;
+import kr.co.amateurs.server.domain.entity.user.User;
 import kr.co.amateurs.server.repository.alarm.AlarmRepository;
+import kr.co.amateurs.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -14,16 +16,21 @@ public class AlarmService {
 
     private final AlarmRepository alarmRepository;
 
-    public AlarmPageResponse readAlarms(long userId, PaginationParam param) {
-        Page<Alarm> page = alarmRepository.findByUserId(userId, param.toPageable());
+    private final UserService userService;
+
+    public AlarmPageResponse readAlarms(PaginationParam param) {
+        User user = userService.getCurrentLoginUser();
+        Page<Alarm> page = alarmRepository.findByUserId(user.getId(), param.toPageable());
         return AlarmPageResponse.from(page);
     }
 
-    public void markAllAsRead(long userId) {
-        alarmRepository.markAllAsReadByUserId(userId);
+    public void markAllAsRead() {
+        User user = userService.getCurrentLoginUser();
+        alarmRepository.markAllAsReadByUserId(user.getId());
     }
 
-    public void markAsRead(long userId, String alarmId) {
-        alarmRepository.markAsReadByUserIdAndId(userId, alarmId);
+    public void markAsRead(String alarmId) {
+        User user = userService.getCurrentLoginUser();
+        alarmRepository.markAsReadByUserIdAndId(user.getId(), alarmId);
     }
 }
