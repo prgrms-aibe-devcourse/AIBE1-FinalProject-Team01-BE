f4eff7dbcb3ff5e9c8e58f1e89e46d86d9411c5a
dldsudqlsdlqslek <tina4686@gmail.com>
2025-06-27 15:52:24 +0900

    Docs: like, bookmark, file, together DTO 스웨거

--- src/main/java/kr/co/amateurs/server/domain/dto/bookmark/BookmarkResponseDTO.java
+++ src/main/java/kr/co/amateurs/server/domain/dto/bookmark/BookmarkResponseDTO.java
@@ -1,6 +1,7 @@
 package kr.co.amateurs.server.domain.dto.bookmark;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.amateurs.server.domain.entity.bookmark.Bookmark;
 import kr.co.amateurs.server.domain.entity.post.GatheringPost;
 import kr.co.amateurs.server.domain.entity.post.MarketItem;
@@ -13,13 +14,22 @@ import java.time.LocalDateTime;
 //TODO - null 사용을 줄이기 위한 인터페이스화
 
 public interface BookmarkResponseDTO {
+    @Schema(description = "게시글 ID", example = "1")
     Long postId();
+    @Schema(description = "게시판 종류", example = "GATHER")
     BoardType boardType();
+    @Schema(description = "게시글 제목", example = "test 제목")
     String title();
+    @Schema(description = "게시글 내용", example = "test 내용")
     String content();
+    @Schema(description = "좋아요 수", example = "1")
     Integer likeCount();
+    @Schema(description = "조회수", example = "3")
     Integer viewCount();
+    @Schema(description = "게시글 태그", example = "Spring Boot")
     String tag();
+    @Schema(description = "생성 일시", example = "2025-06-25T00:08:25")
     LocalDateTime createdAt();
+    @Schema(description = "수정 일시", example = "2025-06-25T00:08:25")
     LocalDateTime updatedAt();
 }
