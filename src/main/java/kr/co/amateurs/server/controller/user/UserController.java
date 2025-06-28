5b1444454146666b6d2e90f81419b13d0cd379aa
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-28 19:02:17 +0900

    Feat: 회원 정보 수정 API 구현

--- src/main/java/kr/co/amateurs/server/controller/user/UserController.java
+++ src/main/java/kr/co/amateurs/server/controller/user/UserController.java
@@ -2,13 +2,15 @@ package kr.co.amateurs.server.controller.user;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import kr.co.amateurs.server.domain.dto.user.UserProfileEditRequestDto;
+import kr.co.amateurs.server.domain.dto.user.UserProfileEditResponseDto;
 import kr.co.amateurs.server.domain.dto.user.UserProfileResponseDto;
 import kr.co.amateurs.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "User", description = "사용자 프로필 관리 API")
 @RestController
@@ -22,6 +24,15 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<UserProfileResponseDto> getCurrentUser() {
         UserProfileResponseDto response = userService.getCurrentUserProfile();
+        return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "내 정보 수정", description = "현재 로그인 한 사용자의 프로필 정보를 수정합니다")
+    @PutMapping("/profile")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<UserProfileEditResponseDto> updateProfile(
+            @Valid @RequestBody UserProfileEditRequestDto request) {
+        UserProfileEditResponseDto response = userService.updateUserProfile(request);
         return ResponseEntity.ok(response);
     }
 }
