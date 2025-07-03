e360fa161fa5a7cba429a07b5dd0909e3102628a
klaus9267 <klaus9267@gmail.com>
2025-07-03 15:41:19 +0900

    Fix: user_not_found의 상태코드가 401인 이슈로 404 에러로 교체

--- src/main/java/kr/co/amateurs/server/service/UserService.java
+++ src/main/java/kr/co/amateurs/server/service/UserService.java
@@ -202,6 +202,6 @@ public class UserService {
 
     public User findById(Long userId) {
         return userRepository.findById(userId)
-                .orElseThrow(ErrorCode.USER_NOT_FOUND);
+                .orElseThrow(ErrorCode.NOT_FOUND);
     }
 }
