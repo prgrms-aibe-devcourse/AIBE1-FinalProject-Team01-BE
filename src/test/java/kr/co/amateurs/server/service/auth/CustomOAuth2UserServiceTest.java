3e5ca9e6214fc4d3eee642d8b3d93155e73cd27c
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-07-03 16:26:10 +0900

    Test: GitHub 필수 필드 누락 예외 테스트 추가

--- src/test/java/kr/co/amateurs/server/service/auth/CustomOAuth2UserServiceTest.java
+++ src/test/java/kr/co/amateurs/server/service/auth/CustomOAuth2UserServiceTest.java
@@ -5,6 +5,7 @@ import kr.co.amateurs.server.config.jwt.CustomUserDetails;
 import kr.co.amateurs.server.domain.entity.user.User;
 import kr.co.amateurs.server.domain.entity.user.enums.ProviderType;
 import kr.co.amateurs.server.domain.entity.user.enums.Role;
+import kr.co.amateurs.server.exception.CustomException;
 import kr.co.amateurs.server.repository.user.UserRepository;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -18,6 +19,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +29,7 @@ import java.time.Instant;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -55,7 +58,7 @@ public class CustomOAuth2UserServiceTest {
     }
 
     @Test
-    void 신규_GitHub_사용자는_회원가입이_가능하다() throws IOException {
+    void 신규_GitHub_사용자는_회원가입이_가능하다() {
         // given
         String githubApiResponse = """
             {
@@ -67,14 +70,8 @@ public class CustomOAuth2UserServiceTest {
             }
             """;
 
-        mockWebServer.enqueue(new MockResponse()
-                .setBody(githubApiResponse)
-                .addHeader("Content-Type", "application/json"));
-
+        enqueueMockResponse(githubApiResponse);
         OAuth2UserRequest userRequest = createGitHubOAuth2UserRequest();
-
-        System.out.println("Mock Server URL: " + mockWebServer.url("/"));
-        System.out.println("UserInfo URI: " + userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri());
 
         // when
         OAuth2User result = customOAuth2UserService.loadUser(userRequest);
@@ -92,6 +89,12 @@ public class CustomOAuth2UserServiceTest {
         assertThat(savedUser.get().getNickname()).startsWith("newuser_");
     }
 
+    private void enqueueMockResponse(String jsonResponse) {
+        mockWebServer.enqueue(new MockResponse()
+                .setBody(jsonResponse)
+                .addHeader("Content-Type", "application/json"));
+    }
+
     private OAuth2UserRequest createGitHubOAuth2UserRequest() {
         String mockServerUrl = mockWebServer.url("/").toString().replaceAll("/$", "");
 
@@ -101,7 +104,7 @@ public class CustomOAuth2UserServiceTest {
                 .authorizationUri(mockServerUrl + "/login/oauth/authorize")
                 .tokenUri(mockServerUrl + "/login/oauth/access_token")
                 .userInfoUri(mockServerUrl + "/user")
-                .userNameAttributeName("id")
+                .userNameAttributeName("login")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUri("http://localhost:8080/login/oauth2/code/github")
                 .build();
@@ -116,8 +119,20 @@ public class CustomOAuth2UserServiceTest {
         return new OAuth2UserRequest(clientRegistration, accessToken);
     }
 
+    private String createGitHubApiResponse(String id, String login, String name, String email) {
+        return String.format("""
+            {
+                "id": %s,
+                "login": "%s",
+                "name": "%s",
+                "email": "%s",
+                "avatar_url": "https://github.com/avatar.jpg"
+            }
+            """, id, login, name, email);
+    }
+
     @Test
-    void 기존_GitHub_사용자는_로그인이_가능하다() throws IOException {
+    void 기존_GitHub_사용자는_로그인이_가능하다() {
         // given
         User existingUser = User.builder()
                 .providerId("12345")
@@ -131,19 +146,16 @@ public class CustomOAuth2UserServiceTest {
         userRepository.save(existingUser);
 
         String githubApiResponse = """
-        {
-            "id": 12345,
-            "login": "newuser",
-            "name": "뉴깃헙",
-            "email": "newuser@github.com",
-            "avatar_url": "https://github.com/avatar.jpg"
-        }
-        """;
+            {
+                "id": 12345,
+                "login": "newuser",
+                "name": "뉴깃헙",
+                "email": "newuser@github.com",
+                "avatar_url": "https://github.com/avatar.jpg"
+            }
+            """;
 
-        mockWebServer.enqueue(new MockResponse()
-                .setBody(githubApiResponse)
-                .addHeader("Content-Type", "application/json"));
-
+        enqueueMockResponse(githubApiResponse);
         OAuth2UserRequest userRequest = createGitHubOAuth2UserRequest();
 
         // when
@@ -160,5 +172,78 @@ public class CustomOAuth2UserServiceTest {
 
         long userCount = userRepository.count();
         assertThat(userCount).isEqualTo(1);
+    }
+
+    @Test
+    void GitHub에서_사용자_ID가_없으면_예외가_발생한다() {
+        // given
+        String responseWithoutId = """
+            {
+                "id": null,
+                "login": "testuser",
+                "name": "Test User",
+                "email": "test@github.com",
+                "avatar_url": "https://github.com/avatar.jpg"
+            }
+            """;
+
+        enqueueMockResponse(responseWithoutId);
+        OAuth2UserRequest userRequest = createGitHubOAuth2UserRequest();
+
+        // when & then
+        assertThatThrownBy(() -> customOAuth2UserService.loadUser(userRequest))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @Test
+    void GitHub에서_로그인_정보가_없으면_예외가_발생한다() {
+        // given
+        String responseWithoutLogin = """
+            {
+                "id": 12345,
+                "name": "Test User",
+                "email": "test@github.com",
+                "avatar_url": "https://github.com/avatar.jpg"
+            }
+            """;
+
+        enqueueMockResponse(responseWithoutLogin);
+        OAuth2UserRequest userRequest = createGitHubOAuth2UserRequest();
+
+        // when & then
+        assertThatThrownBy(() -> customOAuth2UserService.loadUser(userRequest))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("login");
+    }
+
+    @Test
+    void GitHub에서_이메일이_없으면_가짜_이메일이_생성된다() {
+        // given
+        String responseWithoutEmail = """
+            {
+                "id": 55555,
+                "login": "noemail",
+                "name": "메일없는사용자",
+                "avatar_url": "https://noemail.jpg"
+            }
+            """;
+
+        enqueueMockResponse(responseWithoutEmail);
+        OAuth2UserRequest userRequest = createGitHubOAuth2UserRequest();
+
+        // when
+        OAuth2User result = customOAuth2UserService.loadUser(userRequest);
+
+        // then
+        assertThat(result).isInstanceOf(CustomUserDetails.class);
+
+        Optional<User> savedUser = userRepository.findByProviderIdAndProviderType("55555", ProviderType.GITHUB);
+        assertThat(savedUser).isPresent();
+
+        String generatedEmail = savedUser.get().getEmail();
+        assertThat(generatedEmail).startsWith("github_55555@");
+        assertThat(generatedEmail).endsWith("@amateurs.com");
+        assertThat(savedUser.get().getName()).isEqualTo("메일없는사용자");
+        assertThat(savedUser.get().getNickname()).startsWith("noemail_");
     }
 }
