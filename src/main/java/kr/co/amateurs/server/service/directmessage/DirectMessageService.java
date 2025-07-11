0cb55b54e50f7f4d33fc3c3dd0e79b00c813a957
klaus9267 <klaus9267@gmail.com>
2025-07-11 16:27:58 +0900

    Fix: 채팅방 목록 조회 최신 사용순으로 정렬 수정

--- src/main/java/kr/co/amateurs/server/service/directmessage/DirectMessageService.java
+++ src/main/java/kr/co/amateurs/server/service/directmessage/DirectMessageService.java
@@ -15,10 +15,10 @@ import kr.co.amateurs.server.repository.user.UserRepository;
 import kr.co.amateurs.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -32,7 +32,7 @@ public class DirectMessageService {
 
     //test
     private final UserRepository userRepository;
-    
+
     private final Random random = new Random();
 
     @AlarmTrigger(type = AlarmType.DIRECT_MESSAGE)
@@ -62,29 +62,29 @@ public class DirectMessageService {
 
     public DirectMessageRoomResponse createTestRoom() {
         User currentUser = userService.getCurrentLoginUser();
-        
+
         List<Long> existingPartnerIds = directMessageRoomRepository.findAllRoomsByUserId(currentUser.getId()).stream()
                 .flatMap(room -> room.getParticipants().stream())
                 .map(Participant::getUserId)
                 .filter(id -> !id.equals(currentUser.getId()))
                 .distinct()
                 .toList();
-        
+
         List<User> availableUsers = userRepository.findAll().stream()
                 .filter(user -> !user.getId().equals(currentUser.getId()))
                 .filter(user -> !existingPartnerIds.contains(user.getId()))
                 .toList();
-        
+
         if (availableUsers.isEmpty()) {
             throw new CustomException(ErrorCode.INVALID_USER_ID);
         }
-        
+
         User partner = availableUsers.get(random.nextInt(availableUsers.size()));
-        
+
         // 새 채팅방 생성
         List<User> participants = List.of(currentUser, partner);
         DirectMessageRoom newRoom = directMessageRoomRepository.save(DirectMessageRoom.from(participants));
-        
+
         return DirectMessageRoomResponse.fromCollection(newRoom, partner);
     }
 
@@ -95,9 +95,9 @@ public class DirectMessageService {
 
     public List<DirectMessageRoomResponse> getRooms() {
         User currentUser = userService.getCurrentLoginUser();
-        List<DirectMessageRoom> rooms = directMessageRoomRepository.findActiveRoomsByUserId(currentUser.getId());
+        Sort sort = Sort.by(Sort.Direction.DESC, "sentAt");
+        List<DirectMessageRoom> rooms = directMessageRoomRepository.findActiveRoomsByUserId(currentUser.getId(), sort);
         return rooms.stream()
-                .sorted(Comparator.comparing(DirectMessageRoom::getSentAt).reversed())
                 .map(room -> DirectMessageRoomResponse.fromCollection(room, currentUser))
                 .toList();
     }
