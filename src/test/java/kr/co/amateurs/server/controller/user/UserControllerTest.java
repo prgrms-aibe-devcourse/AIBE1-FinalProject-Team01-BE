6f5f12de3e1053b085eea0d6b3b5a3dfde0d8f49
김주엽 <kjyy08@naver.com>
2025-07-15 17:18:35 +0900

    Refactor: 유저 컨트롤러 토큰 관련 테스트 HTTP 응답 코드 수정

--- src/test/java/kr/co/amateurs/server/controller/user/UserControllerTest.java
+++ src/test/java/kr/co/amateurs/server/controller/user/UserControllerTest.java
@@ -116,7 +116,7 @@ public class UserControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void 인증_토큰_없이_기본_정보_수정_요청_시_403_에러가_발생한다() {
+    void 인증_토큰_없이_기본_정보_수정_요청_시_401_에러가_발생한다() {
         // given
         UserBasicProfileEditRequestDTO updateRequest = UserBasicProfileEditRequestDTO.builder()
                 .name("변경된이름")
@@ -129,7 +129,7 @@ public class UserControllerTest extends AbstractControllerTest {
                 .when()
                 .put("/users/me")
                 .then()
-                .statusCode(403);
+                .statusCode(401);
     }
 
     @Test
@@ -261,7 +261,7 @@ public class UserControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    void 인증_토큰_없이_탈퇴_요청_시_403_에러가_발생한다() {
+    void 인증_토큰_없이_탈퇴_요청_시_401_에러가_발생한다() {
         // given
         UserDeleteRequestDTO deleteRequest = new UserDeleteRequestDTO(rawPassword);
 
@@ -272,6 +272,6 @@ public class UserControllerTest extends AbstractControllerTest {
                 .when()
                 .delete("/users/me")
                 .then()
-                .statusCode(403);
+                .statusCode(401);
     }
 }
