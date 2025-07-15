88fa48df4324e4d1e6f320d90bff890b5b027721
rbxo0128 <rbxo0128@naver.com>
2025-07-15 17:12:51 +0900

    Feat: 함께해요 상태 수정 api 추가

--- src/main/java/kr/co/amateurs/server/controller/together/MatchController.java
+++ src/main/java/kr/co/amateurs/server/controller/together/MatchController.java
@@ -8,6 +8,9 @@ import kr.co.amateurs.server.domain.dto.common.PageResponseDTO;
 import kr.co.amateurs.server.domain.dto.together.MatchPostRequestDTO;
 import kr.co.amateurs.server.domain.dto.together.MatchPostResponseDTO;
 import kr.co.amateurs.server.domain.dto.common.PostPaginationParam;
+import kr.co.amateurs.server.domain.entity.post.enums.GatheringStatus;
+import kr.co.amateurs.server.domain.entity.post.enums.MarketStatus;
+import kr.co.amateurs.server.domain.entity.post.enums.MatchingStatus;
 import kr.co.amateurs.server.service.together.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
@@ -57,6 +60,16 @@ public class MatchController {
             @PathVariable("matchId") Long matchId,
             @RequestBody @Valid MatchPostRequestDTO dto){
         matchService.updateMatchPost(matchId, dto);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/{matchId}/{status}")
+    @Operation(summary = "팀원 모집 글 상태 수정", description = "팀원 모집 탭의 본인이 작성한 게시글의 상태를 수정합니다.")
+    public ResponseEntity<Void> updateMarketPostStatus(
+            @PathVariable("matchId") Long matchId,
+            @PathVariable("status") MatchingStatus status
+    ){
+        matchService.updateMatchPostStatus(matchId, status);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
