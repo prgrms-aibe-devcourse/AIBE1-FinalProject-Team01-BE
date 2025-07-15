853ec20887c8b2deefdb55fb0ee04c83b9d0de60
김주엽 <kjyy08@naver.com>
2025-07-15 15:39:52 +0900

    Refactor: 댓글 API HTTP 메서드 별 권한 설정 추가

--- src/main/java/kr/co/amateurs/server/config/auth/http/CommentAuthorizeHttpRequestsConfig.java
+++ src/main/java/kr/co/amateurs/server/config/auth/http/CommentAuthorizeHttpRequestsConfig.java
@@ -1,5 +1,6 @@
 package kr.co.amateurs.server.config.auth.http;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,9 @@ public class CommentAuthorizeHttpRequestsConfig implements CustomAuthorizeHttpRe
     @Override
     public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
         auth
-                .requestMatchers("/api/v1/posts/*/comments/**").permitAll();
+                .requestMatchers(HttpMethod.GET, "/api/v1/posts/*/comments/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/posts/*/comments/**").hasAnyRole("ADMIN", "STUDENT", "GUEST")
+                .requestMatchers(HttpMethod.PUT, "/api/v1/posts/*/comments/**").hasAnyRole("ADMIN", "STUDENT", "GUEST")
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/posts/*/comments/**").hasAnyRole("ADMIN", "STUDENT", "GUEST");
     }
 }
