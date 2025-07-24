4b3a783680eb05ac01736e2638e4ed4d56103b9d
dldsudqlsdlqslek <tina4686@gmail.com>
2025-07-24 21:29:16 +0900

    Chore: S3를 R2로 변경

--- src/main/java/kr/co/amateurs/server/config/S3Config.java
+++ src/main/java/kr/co/amateurs/server/config/S3Config.java
@@ -8,25 +8,32 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
 
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloudflare.r2.endpoint}")
+    private String endpoint;
+
+    @Value("${cloudflare.r2.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloudflare.r2.secret-key}")
     private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
 
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
-                .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of("auto"))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
                 .build();
     }
 }
