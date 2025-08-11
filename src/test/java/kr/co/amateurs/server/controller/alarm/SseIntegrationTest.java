68c3542dcae84eff420775c6ebb6f8bf251516c8
kjyy08 <kjyy08@naver.com>
2025-08-11 22:48:11 +0900

    Test: SSE 통합 테스트에 연결 대기 로직 추가

--- src/test/java/kr/co/amateurs/server/controller/alarm/SseIntegrationTest.java
+++ src/test/java/kr/co/amateurs/server/controller/alarm/SseIntegrationTest.java
@@ -15,10 +15,12 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 @Sql(scripts = "/reset.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 @Sql(scripts = "/reset.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
@@ -103,6 +105,9 @@ class SseIntegrationTest extends AbstractControllerTest {
                 .get("/sse")
                 .then()
                 .statusCode(HttpStatus.OK.value());
+
+        await().atMost(Duration.ofSeconds(2))
+                .until(() -> getConnections().size() == 1);
 
         given().header("Authorization", "Bearer " + accessToken)
                 .accept(MediaType.TEXT_EVENT_STREAM_VALUE)
